c0dba1e2fc0cf3bd67e43ee7bd122d4cde6d44bf
ddorangie <nuvelax02232@gmail.com>
2025-02-08 15:30:52 +0300

    \

--- 3.js
+++ 3.js
@@ -6,5 +6,18 @@ const arr2 = [1, 2, 4, 7, 8];
 // должно получиться [2,3,5,7,8]
 
 
-const arr3 = 
+const arr3 = arr1.concat(arr2)
+console.log(arr3)
 
+let result = arr3.sort()
+console.log(result)
+let newArr = []
+
+for (let i = 0; i < arr3.length; i++) {
+    
+    if (arr3[i] > arr3[i - 1] || arr3[i] < arr3[i-1]) {
+        newArr.push(arr3[i])
+    }
+}
+
+console.log(newArr)
